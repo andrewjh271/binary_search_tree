9571defee987ff22e987a98f37a0011f997f0fdd
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-06-02 23:18:21 -0700

    All tree functions seem to be working well. Also added a display method for up to 5 levels

--- tree.rb
+++ tree.rb
@@ -8,8 +8,8 @@ class Tree
   attr_accessor :root
 
   def initialize(array)
-    @array = array.sort.uniq
-    @root = build_tree(@array)
+    # @array = array.sort.uniq
+    @root = build_tree(array.sort.uniq)
   end
 
   def empty?
@@ -22,16 +22,57 @@ class Tree
     mid = arr.length / 2
     root = Node.new(arr[mid])
     root.left = build_tree(arr.take(mid))
-    root.right = build_tree(arr.drop(mid + 1))
+    root.right = build_tree(arr.drop(mid + 1)) unless arr.drop(mid + 1).empty?
     root
+  end
+
+  def display
+    levels = depth_recursion(@root)
+    if levels > 5
+      puts "Can only display up to 5 levels. Current # of levels: #{levels}"
+      return
+    end
+    66.times { print '-' }
+    2.times { puts }
+    queue = []
+    return if empty?
+    blank = Node.new('--')
+    blank.left = blank
+    blank.right = blank
+
+    level = 0
+    i = 0
+    n_blanks = 32
+    n_blanks.times { print ' ' }
+    queue << @root
+    until queue.empty?
+      
+      current = queue.shift
+      queue << (current.left ? current.left : blank)
+      queue << (current.right ? current.right : blank)
+      print current.value
+      (n_blanks * 2 - 2).times { print ' ' }
+      
+      i += 1
+      if i == 2**(level + 1) - 1
+        puts
+        break if queue.all?(blank)
+        level += 1
+        n_blanks /= 2
+        n_blanks.times { print ' ' }
+        
+      end
+    end
+    puts
+    66.times { print '-' }
+    puts
   end
 
   def insert(value)
     new_node = Node.new(value)
     if empty?
       @root = new_node
-    elsif @array.include?(value)
-      # this should be changed to use tree traversal
+    elsif find(value)
       false
     else
       current = @root
@@ -90,7 +131,6 @@ class Tree
 
     queue << @root
     until queue.empty?
-      # binding.pry
       current = queue.shift
       queue << current.left unless current.left.nil?
       queue << current.right unless current.right.nil?
@@ -113,9 +153,51 @@ class Tree
 
   def in_order
     return [] if empty?
+
     array = []
     in_order_rec(@root, array)
-    array
+    return array unless block_given?
+
+    array.each { |item| yield item }
+  end
+
+  def pre_order
+    return [] if empty?
+
+    array = []
+    pre_order_rec(@root, array)
+    return array unless block_given?
+
+    array.each { |item| yield item }
+  end
+
+  def post_order
+    return [] if empty?
+
+    array = []
+    post_order_rec(@root, array)
+    return array unless block_given?
+
+    array.each { |item| yield item }
+  end
+
+  # Node class is only visible to Tree class
+  def depth(key)
+    key_node = find(key)
+    return false unless key_node
+
+    depth_recursion(key_node)
+  end
+
+  def balanced?
+    return false if empty?
+
+    balanced_rec(@root)
+  end
+
+  def rebalance
+    array = level_order
+    @root = build_tree(array.sort)
   end
 
   private
@@ -149,10 +231,46 @@ class Tree
 
   def in_order_rec(local_root, array)
     return if local_root.nil?
-    left_tree = in_order_rec(local_root.left, array)
-    array << left_tree if left_tree
+
+    in_order_rec(local_root.left, array)
     array << local_root.value
-    right_tree = in_order_rec(local_root.right, array)
-    array << right_tree if right_tree
+    in_order_rec(local_root.right, array)
+  end
+
+  def pre_order_rec(local_root, array)
+    return if local_root.nil?
+
+    array << local_root.value
+    pre_order_rec(local_root.left, array)
+    pre_order_rec(local_root.right, array)
+  end
+
+  def post_order_rec(local_root, array)
+    return if local_root.nil?
+
+    post_order_rec(local_root.left, array)
+    post_order_rec(local_root.right, array)
+    array << local_root.value
+  end
+
+  def depth_recursion(local_root)
+    return -1 if local_root.nil?
+
+    left_depth = 1 + depth_recursion(local_root.left)
+    right_depth = 1 + depth_recursion(local_root.right)
+    left_depth > right_depth ? left_depth : right_depth
+  end
+
+  def balanced_rec(local_root)
+    return false unless (depth_recursion(local_root.left) - 
+                        depth_recursion(local_root.right)).abs <= 1
+    
+    if local_root.left
+      return false unless balanced_rec(local_root.left) 
+    end
+    if local_root.right
+      return false unless balanced_rec(local_root.right)
+    end
+    true
   end
 end
--- tree_app.rb
+++ tree_app.rb
@@ -5,23 +5,26 @@ require_relative 'tree'
 # Tests binary search tree
 class TreeApp
   
+  tree = Tree.new(Array.new(20) { rand(1...100) })
 
-
-  tree = Tree.new([0, 1, 2, 3, 4, 5, 6])
-  # tree = Tree.new([3, 5, 4, 2, 7, 8, 9, 11, 15, 13, 12])
-  tree.insert(4.5)
-  tree.insert(1.5)
-  # tree.delete(5)
-  p tree.root
-  # p tree.find(2)
+  tree.display
   
-  # p tree.level_order
-  # tree.level_order { |data| puts data + 2 }
+  puts "Balanced? #{tree.balanced?}"
+  puts "Preorder: #{tree.pre_order}"
+  puts "Inorder: #{tree.in_order}"
+  puts "Postorder: #{tree.post_order}"
+  puts "Level order: #{tree.level_order}"
 
-  # p tree.level_order_alt
-  # tree.level_order_alt { |data| puts data + 2 }
-
-  p tree.in_order
-
+  7.times { tree.insert(rand(100...150)) }
+  
+  tree.display
+  puts "Balanced? #{tree.balanced?}"
+  tree.rebalance
+  tree.display
+  puts "Balanced? #{tree.balanced?}"
+  puts "Preorder: #{tree.pre_order}"
+  puts "Inorder: #{tree.in_order}"
+  puts "Postorder: #{tree.post_order}"
+  puts "Level order: #{tree.level_order}"
 
 end
